b3de468f0d7ce6ccfd860e07bed6f6d3aa7316b5
Shikage5 <gabriel.salajeanu@gmail.com>
2023-11-02 13:07:03 +0100

    Base

--- cmd/main.go
+++ cmd/main.go
@@ -1,23 +1,5 @@
 package main
 
-import (
-	"NutritionCalculator/pkg/handlers"
-	"log"
-	"net/http"
-)
-
-func mainHandler(w http.ResponseWriter, r *http.Request) {
-
-	responseString := "<html><body>Hello WORLD</body></html>"
-	w.Write([]byte(responseString)) // unbedingt Templates verwenden!
-}
-
 func main() {
 
-	http.HandleFunc("/", handlers.IndexHandler)
-	http.HandleFunc("/login", handlers.LoginHandler)
-	http.HandleFunc("/register", handlers.RegisterHandler)
-
-	log.Println("Server is running on :8080...")
-	log.Fatalln(http.ListenAndServe(":8080", nil))
 }
