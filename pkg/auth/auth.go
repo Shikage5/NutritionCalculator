e03eedb2dbbda69f147c3268a5f7205a68acc8e1
Shikage5 <gabriel.salajeanu@gmail.com>
2023-11-07 15:04:40 +0100

    Auth function implemented

--- pkg/auth/auth.go
+++ pkg/auth/auth.go
@@ -2,8 +2,24 @@ package auth
 
 import (
 	"NutritionCalculator/data/models"
+	"errors"
 )
 
-func Auth(user models.User, mockUsers []models.User) (bool, error) {
+func Auth(inputUser models.User, userList []models.User) (bool, error) {
+	// Validate input
+	if inputUser.Username == "" || inputUser.PasswordHash == "" {
+		return false, errors.New("username and password cannot be empty")
+	}
 
+	for _, u := range userList {
+		if inputUser.Username == u.Username {
+			// User found, check password hash
+			if inputUser.PasswordHash == u.PasswordHash {
+				return true, nil // User exists with the correct password
+			}
+			return false, nil // User found, but incorrect password
+		}
+	}
+
+	return false, nil // User not found
 }
