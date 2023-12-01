9aad31d43b87c558e1516e661997e61d2b4d863a
Shikage5 <gabriel.salajeanu@gmail.com>
2023-12-01 22:22:05 +0100

    Refactor login_test.go to improve code readability

--- pkg/handlers/login_test.go
+++ pkg/handlers/login_test.go
@@ -7,7 +7,7 @@ import (
 	"bytes"
 	"context"
 	"encoding/json"
-	"errors"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -15,70 +15,46 @@ import (
 
 func TestLoginHandler(t *testing.T) {
 	testCases := []struct {
-		desc                string
-		reqMethod           string
-		reqURL              string
-		reqBody             []byte
-		expectedStatusCode  int
-		expectedBody        string
-		mockAuthShouldFail  bool
-		mockAuthShouldError bool
+		desc               string
+		reqMethod          string
+		reqURL             string
+		reqBody            []byte
+		expectedStatusCode int
+		expectedBody       string
+		mockAuthShouldFail bool
 	}{
 		{
-			desc:                "GET request should return 200 OK",
-			reqMethod:           http.MethodGet,
-			reqURL:              "/login",
-			reqBody:             nil,
-			expectedStatusCode:  http.StatusOK,
-			expectedBody:        "Login form",
-			mockAuthShouldFail:  false,
-			mockAuthShouldError: false,
+			desc:               "GET request should return 200 OK",
+			reqMethod:          http.MethodGet,
+			reqURL:             "/login",
+			reqBody:            nil,
+			expectedStatusCode: http.StatusOK,
+			expectedBody:       "Login form",
+			mockAuthShouldFail: false,
 		},
 		{
-			desc:                "POST request with valid credentials should return 200 OK",
-			reqMethod:           http.MethodPost,
-			reqURL:              "/login",
-			reqBody:             []byte(`{"username": "testuser", "password": "testpassword"}`),
-			expectedStatusCode:  http.StatusOK,
-			expectedBody:        "Login successful",
-			mockAuthShouldFail:  false,
-			mockAuthShouldError: false,
+			desc:               "POST request with valid credentials should return 200 OK",
+			reqMethod:          http.MethodPost,
+			reqURL:             "/login",
+			reqBody:            []byte(`{"username": "testuser", "password": "testpassword"}`),
+			expectedStatusCode: http.StatusOK,
+			expectedBody:       "Login successful",
+			mockAuthShouldFail: false,
 		},
 		{
-			desc:                "POST request with invalid credentials should return 401 Unauthorized",
-			reqMethod:           http.MethodPost,
-			reqURL:              "/login",
-			reqBody:             []byte(`{"username": "testuser", "password": "wrongpassword"}`),
-			expectedStatusCode:  http.StatusUnauthorized,
-			expectedBody:        "invalid credentials",
-			mockAuthShouldFail:  true,
-			mockAuthShouldError: false,
-		},
-		{
-			desc:                "POST request with missing username should return 400 Bad Request",
-			reqMethod:           http.MethodPost,
-			reqURL:              "/login",
-			reqBody:             []byte(`{"password": "testpassword"}`),
-			expectedStatusCode:  http.StatusBadRequest,
-			expectedBody:        "Missing username or password",
-			mockAuthShouldFail:  false,
-			mockAuthShouldError: true,
-		},
-		{
-			desc:                "POST request with missing password should return 400 Bad Request",
-			reqMethod:           http.MethodPost,
-			reqURL:              "/login",
-			reqBody:             []byte(`{"username": "testuser"}`),
-			expectedStatusCode:  http.StatusBadRequest,
-			expectedBody:        "Missing username or password",
-			mockAuthShouldFail:  false,
-			mockAuthShouldError: true,
+			desc:               "POST request with invalid credentials should return 401 Unauthorized",
+			reqMethod:          http.MethodPost,
+			reqURL:             "/login",
+			reqBody:            []byte(`{"username": "testuser", "password": "wrongpassword"}`),
+			expectedStatusCode: http.StatusUnauthorized,
+			expectedBody:       "invalid credentials\n",
+			mockAuthShouldFail: true,
 		},
 	}
 
 	for _, tC := range testCases {
 		t.Run(tC.desc, func(t *testing.T) {
-			mockAuthService := &mockAuthService{shouldFail: tC.mockAuthShouldFail, shouldError: tC.mockAuthShouldError}
+			mockAuthService := &mockAuthService{shouldFail: tC.mockAuthShouldFail}
 
 			var userRequest UserRequest
 			json.Unmarshal(tC.reqBody, &userRequest)
@@ -100,6 +76,8 @@ func TestLoginHandler(t *testing.T) {
 				t.Errorf("Expected status code %d, got %d", tC.expectedStatusCode, rr.Code)
 			}
 			if rr.Body.String() != tC.expectedBody {
+				fmt.Printf("Actual body: '%s'\n", rr.Body.String())
+				fmt.Printf("Expected body: '%s'\n", tC.expectedBody)
 				t.Errorf("Expected body %s, got %s", tC.expectedBody, rr.Body.String())
 			}
 		})
@@ -107,16 +85,12 @@ func TestLoginHandler(t *testing.T) {
 }
 
 type mockAuthService struct {
-	shouldFail  bool
-	shouldError bool
+	shouldFail bool
 }
 
-func (m mockAuthService) Auth(user models.User) (bool, error) {
+func (m mockAuthService) Auth(inputUser models.User) (bool, error) {
 	if m.shouldFail {
 		return false, auth.ErrInvalidCredentials
-	}
-	if m.shouldError {
-		return false, errors.New("error")
 	}
 	return true, nil
 }
